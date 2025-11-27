2f4df29fd42c894ea4c867a76d5bbaa44902e314
Elijah Gleckler <elik12g@gmail.com>
2025-11-27 14:46:28 -0500

    enhancing visualization capabilities for solution

--- boundary.py
+++ boundary.py
@@ -271,9 +271,9 @@ class Boundary:
         for ip, idx in self.interior_idx.items():
             c = int(ip[0] + (self.n - 1) / 2)  # x-axis goes left --> right
             r = (self.n - 1) - int(ip[1] + (self.n - 1) / 2)  # y-axis goes bottom --> top
-            red_window[r][c] = 0
-            green_window[r][c] = 255 - normalized_ivs[idx]
-            blue_window[r][c] = normalized_ivs[idx]
+            red_window[r][c] = int(0.5 * normalized_ivs[idx])
+            green_window[r][c] = 128 - int(0.5 * normalized_ivs[idx])
+            blue_window[r][c] = 128
 
         # Mark the origin
         o = int((self.n - 1) / 2)
